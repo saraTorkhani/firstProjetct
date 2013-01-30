e0f43246a4d5b816c82046cbdb61bd85441a0fa6
takwa2015 <takwa2015@172.16.200.137>
2013-01-30 11:26:15 +0100

    Methnani commit

--- tn.edu.espritEntreprise.firstSteps/src/tn/edu/espritEntreprise/firstSteps/saraClasses/Wtiya.java
+++ tn.edu.espritEntreprise.firstSteps/src/tn/edu/espritEntreprise/firstSteps/saraClasses/Wtiya.java
@@ -6,8 +6,8 @@ public class Wtiya {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
 	}
 
 }
---
+++ tn.edu.espritEntreprise.firstSteps/src/tn/edu/espritEntreprise/firstSteps/saraClasses/medWork.java
@@ -0,0 +1,11 @@
+package tn.edu.espritEntreprise.firstSteps.saraClasses;
+
+public class medWork {
+
+public static void main(String[] args) {
+	 int a=12;
+		System.out.println("this is a "+a);
+		
+	}
+	
+}

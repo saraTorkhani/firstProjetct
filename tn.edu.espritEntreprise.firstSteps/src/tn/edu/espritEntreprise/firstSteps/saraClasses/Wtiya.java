e5bfbd37f697948bdd0254a9599894a5cf3e3000
sara <sara@sarouchka>
2013-01-31 10:59:45 +0100

    My second commit
    Signed-off-by: sara <sara@sarouchka>

---
+++ tn.edu.espritEntreprise.firstSteps/src/tn/edu/espritEntreprise/firstSteps/saraClasses/Wtiya.java
@@ -0,0 +1,13 @@
+package tn.edu.espritEntreprise.firstSteps.saraClasses;
+
+public class Wtiya {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
